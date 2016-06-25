ceb0bbf4233445d014eaef60893101f3df2a1b72
master <oleg.cherniy@gmail.com>
2016-06-25 11:58:12 +0300

    - Fix normalize function - Add count param

--- mnist_dl.js
+++ mnist_dl.js
@@ -1,17 +1,19 @@
 "use strict"
 const co = require('co'),
+      argv = require('optimist').argv,
       labelsLoader = require('./lib/labelsLoader'),
       digitsLoader = require('./lib/digitsLoader'),
       rawMaker     = require('./lib/rawMaker'),
       rawWriter    = require('./lib/rawWriter');
 
 co(function* () {
-    let labels = yield labelsLoader();
-    let digits = yield digitsLoader();
+    let labels = yield labelsLoader(),
+        count = argv.count || labels.length,
+        digits = yield digitsLoader();
     console.log(labels.length);
     console.log(digits.length);
     // console.log(digits.length/(28*28));
-    let raw   =  rawMaker(labels,digits);
+    let raw   =  rawMaker(labels,digits,count);
     rawWriter(raw);
     
 
